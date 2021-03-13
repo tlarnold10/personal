7459cea52f93074b1914ec1ba7ce27f96572d0d1
tlarnold10 <tlarnold10@gmail.com>
2021-03-13 14:59:14 -0600

    added navigation

---
+++ personal_site/src/Nav.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav className="uk-navbar">
+        <ul className="uk-navbar-nav">
+            <li className="uk-active"><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Experience</a></li>
+            <li><a href="">Projects</a></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
--- personal_site/src/index.tsx
+++ personal_site/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nav from './Nav';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
@@ -9,6 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Nav />
       <App />
     </Provider>
   </React.StrictMode>,
